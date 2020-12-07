e847e8059a50caed62e5f33ca58888c91dcd5274
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2020-12-07 13:22:52 +0000

    Solve day 7 BFS/BDS

--- days/day7.py
+++ days/day7.py
@@ -1,28 +1,37 @@
-def parse_rule(line: str):
-    container, containee = line.split(' bags contain ')
+import collections
+import functools
 
-    if containee == 'no other bags.':
+from data.day7_data import data
+
+
+def parse_rule(line: str):
+    container, containee = line.split(" bags contain ")
+
+    if containee == "no other bags.":
         return container, []
 
     bags = []
-    for c in containee.split(', '):
-        words = c.split(' ')
+    for c in containee.split(", "):
+        words = c.split(" ")
         quantity = int(words[0])
-        bag = ' '.join(words[1:3])
+        bag = " ".join(words[1:3])
         bags.append((quantity, bag))
 
     return container, bags
 
 
 def build_is_contained_by(rules):
-    import collections
     contained_by = collections.defaultdict(set)
 
     for container, content in rules:
-        for bag in content:
+        for _, bag in content:
             contained_by[bag].add(container)
 
     return contained_by
+
+
+def build_contain(rules):
+    return {container: set(content) for container, content in rules}
 
 
 def all_valid_bags(target, contained_by):
@@ -37,6 +46,35 @@ def all_valid_bags(target, contained_by):
 
         valid_bags.add(candidate)
         seen.add(candidate)
-        to_visit.append({b for b in contained_by[candidate] if b not in seen})
+        to_visit.extend({b for b in contained_by[candidate] if b not in seen})
 
     return valid_bags
+
+
+def how_many_bags(target, contain):
+    @functools.lru_cache()
+    def how_many(target):
+        to_visit = list(contain[target])
+        count = 1
+
+        while to_visit:
+            number, bag = to_visit.pop()
+            count += number * how_many(bag)
+
+        return count
+
+    # -1 because we don't want to count the first bag!
+    return how_many(target) - 1
+
+
+def solve_day7():
+    print("Day 7", end=" ")
+
+    rules = [parse_rule(x) for x in data]
+    is_contained_by = build_is_contained_by(rules)
+    can_contain_my_bag = all_valid_bags("shiny gold", is_contained_by)
+
+    print(len(can_contain_my_bag), end=" ")
+
+    contain = build_contain(rules)
+    print(how_many_bags("shiny gold", contain))
--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from days.day3 import solve_day3
 from days.day4 import solve_day4
 from days.day5 import solve_day5
 from days.day6 import solve_day6
+from days.day7 import solve_day7
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solve_day1()
     solve_day2()
     solve_day3()
     solve_day4()
     solve_day5()
     solve_day6()
+    solve_day7()
